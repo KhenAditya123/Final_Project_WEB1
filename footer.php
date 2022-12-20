1753a7e5165a4c9c6aa9df0dd461556adbd8867c
KhenAditya123 <Khenadityaa03@gmail.com>
2022-12-20 20:32:32 +0700

    baru

---
+++ footer.php
@@ -0,0 +1,109 @@
+      <!-- Footer -->
+      <footer class="text-center text-lg-start bg-light text-muted">
+        <!-- Section: Social media -->
+        <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+          <!-- Left -->
+          <div class="me-5 d-none d-lg-block">
+            <span>Get connected with me on social networks:</span>
+          </div>
+          <!-- Left -->
+
+          <!-- Right -->
+          <div>
+            <a href="https://www.facebook.com/khen.aditya.9" class="me-4 link-secondary">
+              <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="https://twitter.com/KhenAditya10" class="me-4 link-secondary">
+              <i class="fab fa-twitter"></i>
+            </a>
+            <a href="mailto:Khen.412021018@civitas.ukrida.ac.id" class="me-4 link-secondary">
+              <i class="fab fa-google"></i>
+            </a>
+            <a href="https://www.instagram.com/angkhentang/" class="me-4 link-secondary">
+              <i class="fab fa-instagram"></i>
+            </a>
+            <a href="" class="me-4 link-secondary">
+              <i class="fab fa-linkedin"></i>
+            </a>
+            <a href="https://github.com/KhenAditya123" class="me-4 link-secondary">
+              <i class="fab fa-github"></i>
+            </a>
+          </div>
+          <!-- Right -->
+        </section>
+        <!-- Section: Social media -->
+
+        <!-- Section: Links  -->
+        <section class="">
+          <div class="container text-center text-md-start mt-5">
+            <!-- Grid row -->
+            <div class="row mt-3">
+              <!-- Grid column -->
+              <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                <!-- Content -->
+                <h6 class="text-uppercase fw-bold mb-4">
+                  <i class="fas fa-gem me-3 text-secondary"></i>JAVANESE CULINARY
+                </h6>
+                <p>
+                  Website Javanese Culinary menyedikan berbagai menu sajian Nusantara dari Pulau Jawa. Mulai Dari
+                  Provinsi
+                  Banten, Provinsi Jawa Barat, Provinsi Jawa Tengah, Provinsi Daerah Istimewa Yogyakarta, dan Provinsi
+                  Jawa Timur.
+                </p>
+              </div>
+              <!-- Grid column -->
+
+
+              <!-- Grid column -->
+              <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                <!-- Links -->
+                <h6 class="text-uppercase fw-bold mb-4">
+                  Provinsi
+                </h6>
+                <p>
+                  <a href="Banten.html" class="text-reset">BANTEN</a>
+                </p>
+                <p>
+                  <a href="dki.html" class="text-reset">DKI JAKARTA</a>
+                </p>
+                <p>
+                  <a href="jabar.html" class="text-reset">JAWA BARAT</a>
+                </p>
+                <p>
+                  <a href="jateng.html" class="text-reset">JAWA TENGAH</a>
+                </p>
+                <p>
+                  <a href="diy.html" class="text-reset">DI YOGYAKARTA</a>
+                </p>
+                <p>
+                  <a href="jatim.html" class="text-reset">JAWA TIMUR</a>
+                </p>
+              </div>
+              <!-- Grid column -->
+
+              <!-- Grid column -->
+              <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                <!-- Links -->
+                <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+                <p><i class="fas fa-home me-3 text-secondary"></i> Tambora,Jembatan Lima</p>
+                <p>
+                  <i class="fas fa-envelope me-3 text-secondary"></i>
+                  Khenadityaa03@gmail.com
+                </p>
+                <p><i class="fas fa-phone me-3 text-secondary"></i> +6285959923325</p>
+              </div>
+              <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
+          </div>
+        </section>
+        <!-- Section: Links  -->
+
+        <!-- Copyright -->
+        <div class="text-center p-4" style="background-color: rgba(247, 119, 119, 0.025);">
+          © 2022 Copyright:
+          <a class="text-reset fw-bold">Khen Aditya</a>
+        </div>
+        <!-- Copyright -->
+      </footer>
+      <!-- Footer -->
